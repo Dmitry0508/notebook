11a363509cb670961dd54536e362a5c35ba79f5c
Dmitry0508 <123839537+Dmitry0508@users.noreply.github.com>
2024-05-29 13:53:37 +0300

    Add files via upload
    Signed-off-by: Dmitry0508 <123839537+Dmitry0508@users.noreply.github.com>

---
+++ A  first plot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from pylab import *
+x=linspace(0,5,10)
+y=x**2
+
+
+# In[3]:
+
+
+figure()
+plot(x,y,'forestgreen')
+xlabel('x')
+ylabel('y')
+title('title')
+show()
+
+
+# In[ ]:
+
+
+
+
